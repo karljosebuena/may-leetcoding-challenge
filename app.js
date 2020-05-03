04af5bbd281d690f94bde4a428b4b464813b9a1f
Karl Jose Buena <karljosebuen@gmail.com>
2020-05-03 12:11:15 +0800

    Added app.js as entryt. poin

---
+++ app.js
@@ -0,0 +1,4 @@
+// Instance of solution to Jewel and Stone problem
+const jewelsAndStones = require('./week-1-may-1st-may-7th/jewels-and-stones')();
+const jewels = jewelsAndStones.numJewelsInStones('aA', 'aAAbbbb');
+console.log(jewels);
