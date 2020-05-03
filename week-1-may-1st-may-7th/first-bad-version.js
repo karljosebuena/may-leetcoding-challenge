1e489072817c4fbd73e1e9317a969261ea7016f5
Karl Jose Buena <karljosebuen@gmail.com>
2020-05-03 16:34:25 +0800

    No need to import/export first bad version solution.

--- week-1-may-1st-may-7th/first-bad-version.js
+++ week-1-may-1st-may-7th/first-bad-version.js
@@ -18,36 +18,35 @@
 // call isBadVersion(4) -> true
 
 // Then 4 is the first bad version.
-module.exports = () => {
-    /**
-     * Definition for isBadVersion()
-     *
-     * @param {integer} version number
-     * @return {boolean} whether the version is bad
-     * isBadVersion = function(version) {
-     *     ...
-     * };
-     */
 
+/**
+ * Definition for isBadVersion()
+ *
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function (isBadVersion) {
     /**
-     * @param {function} isBadVersion()
-     * @return {function}
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
      */
-    var solution = function (isBadVersion) {
-        /**
-         * @param {integer} n Total versions
-         * @return {integer} The first bad version
-         */
-        return function (n) {
-            let left = 1;
-            let right = n;
-            let mid = 0;
-            while (left < right) {
-                mid = left + (right - left) / 2;
-                if (isBadVersion(mid)) right = mid;
-                else left = mid + 1
-            }
-            return right;
-        };
+    return function (n) {
+        let left = 1;
+        let right = n;
+        let mid = 0;
+        while (left < right) {
+            mid = left + (right - left) / 2;
+            if (isBadVersion(mid)) right = mid;
+            else left = mid + 1
+        }
+        return right;
     };
-}
+};
